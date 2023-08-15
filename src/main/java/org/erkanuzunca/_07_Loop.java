9aecbf72c213a42c3a793ac43b84fb51e83f6b79
erkanuzunca <erkan_uzunca@hotmail.com>
2023-08-15 20:03:25 +0300

    Methods,Enum,Diziler,Class

---
+++ src/main/java/org/erkanuzunca/_07_Loop.java
@@ -0,0 +1,29 @@
+package org.erkanuzunca;
+
+public class _07_Loop {
+
+    public static void main(String[] args) {
+
+       for (int i=0;i<10;i++){
+           System.out.print(i+" ");
+       }
+        System.out.println("\n********************");
+
+       int k =0;
+       while (k<10){
+           System.out.print(k+" ");
+           k++;
+       }
+        System.out.println("\n********************");
+       int m=0;
+       do{
+           System.out.print(m+" ");
+           m++;
+       }
+        while(m<10);
+
+
+
+    }
+
+}
